e2bf3f1ff9362fab167bc9273dae8c6d572b37f6
AndreaAKApax <45388849+AndreaAKApax@users.noreply.github.com>
2019-07-18 13:58:02 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <collectc/hashtable.h>
+#include <sys/stat.h>
+
+
+
+int main()
+{
+    HashTable *table;
+    enum cc_stat status = hashtable_new(&table);
+    if (status == CC_OK)
+        printf("table creation ok\n");
+
+    struct stat array[3];
+    int i;
+    for(i = 0; i<3; i++){
+        array[i] = struct stat filestat;
+        }
+
+
+void addToHT (char *pathname){
+    static int index = 0;
+
+    int statStatus;
+    if((statStatus = stat(pathname, &(array[index])) != 0)){
+        fprintf(stderr, "errore con il metodo stat");
+        exit(1);
+        }
+
+    printf("file serial number = %d", array[index].st_ino);
+
+    if((hashtable_add(table, pathname, array[index])) != CC_OK){
+        fprintf(stderr, "errore nell'inserire la coppia in tabella");
+        exit(1);
+        }
+
+    void *value;
+    if((hashtable_get(table, pathname, &value) != CC_OK){
+        fprintf(stderr, "errore con il metodo get");
+        exit(1);
+        }
+
+    fprintf("file serial number = %d" value.st_ino);
+
+    index++;
+};
+
+    addToHT("/home/andrea/Scrivania/file2");
+    addToHT("/home/andrea/Scrivania/super");
+    addToHT("/home/andrea/Scrivania/file1");
+
+    return 0;
+}
